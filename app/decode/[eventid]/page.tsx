be4d00857c2a42acf725ac0a760ef4f6fc130577
ihouston2137 <ian.houston.torc@gmail.com>
2025-10-21 17:59:47 -0400

    Working on Event Page

--- app/decode/[eventid]/page.tsx
+++ app/decode/[eventid]/page.tsx
@@ -26,6 +26,15 @@ interface EventItem
         ],
     }
 
+    function returnDate(date: string) {
+        const dateObj = new Date(date);
+        const month = dateObj.getMonth() + 1;
+        const day = dateObj.getDate();
+        const year = dateObj.getFullYear();
+        return month + "/" + day+ "/" + year;
+    }
+
+
 export default function Page({ params, }: { params: Promise<{ eventid: string }>}){
     const { eventid } = use(params)
     const [event, setEvent] = useState<EventItem>({code: 'loading', regionCode: '', name: '', typeName: '', city: '', stateprov: '', country: '', weekStart: '', dateStart: '', dateEnd: '', teamsCount: 0, teamsList: [{teamNumber: 0, nameShort: '', schoolName: '', city: '', stateProv: '', country: ''},]});
@@ -45,6 +54,24 @@ export default function Page({ params, }: { params: Promise<{ eventid: string }>
   return (
     <div className='text-black p-2'>
         <h1 className="text-2xl font-black">{event.name}</h1>
+        <p className="text-sm font-normal">{event.typeName} | {event.city}, {event.stateprov}, {event.country} | 
+            {returnDate(event.dateStart)} to {returnDate(event.dateEnd)}</p>
+        <p className="text-sm font-normal">{event.teamsList.length} Teams Participating</p>
+        <div className="flex flex-row gap-1 flex-wrap">
+            {event.teamsList.map((team, index: number) => (
+                <div key={index} className="flex flex-col w-[100%] sm:w-[48%] lg:w-[33%] xl:w-[24%] p-1">
+
+                    <div className="flex flex-col bg-slate-600 rounded-lg p-1 shadow-lg">
+                        <h2 className="text-sm font-bold text-slate-200 pl-2">TEAM {team.teamNumber}</h2>
+                        <div className="flex flex-row gap-2 p-2 w-[100%] bg-slate-100 mb-1 rounded-md">
+                            <div className="min-w-[12%]"><div className="text-xs font-light bg-slate-900 rounded-lg p-0.5 text-white text-center">{team.teamNumber}</div></div>
+                            <div className="text-sm font-bold">{team.nameShort}</div>
+                            <div className="text-xs font-normal">{team.schoolName}</div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
         
     </div>
   )
